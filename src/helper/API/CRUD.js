b9c4f87457f70e45d17e00b24c952d48b78e50b8
James Chen <james.chen@nyu.edu>
2019-08-19 16:16:32 -0400

    added helper functions to handle toggle

---
+++ src/helper/API/CRUD.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import axios from 'axios';
+
+
+export default function GET() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default function DELETE() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default function POST() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default function PUT() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/helper/helper.js
@@ -0,0 +1,67 @@
+import React from 'react'
+
+
+// update state when inputing texts
+export const = function (key, value, objKey) {
+    this.setState(            
+        prevState => {
+            let newCompany = Object.assign({}, prevState.newCompany);
+            newCompany.name = "a";
+            // else if(id == 'Catalog') newCompany.catalog = "b";
+            // else {
+            //     newCompany.status = value;
+            //     this.statusHandleChange(value);
+            // }
+            return {newCompany};
+        }
+    );
+}
+
+// update state when a swtich is invoke
+export const handletoggleChange = function (key, value){
+    const stateKey = key;
+    const stateVal = value;
+    this.setState({
+        [stateKey]: !stateVal
+    }, () => {
+        console.log(this.state);
+    })
+}
+
+// add a resource entity 
+export function addEntity(store_hash, func, payload){
+    return (
+    <div></div>
+    )
+}
+
+// get a resource entity 
+export function getEntity(store_hash, func, payload){
+    return (
+    <div></div>
+    )
+}
+
+// get a resource collection 
+export function getCollection(store_hash, func, payload){
+    return (
+    <div></div>
+    )
+}
+
+// delete a resource entity 
+export function deleteEntity(store_hash, func, payload){
+    return (
+    <div></div>
+    )
+}
+
+// update a resource entity
+export function udpateEntity(){
+    return (
+        <div></div>
+    )
+}
+
+
+
