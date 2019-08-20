cc92f4063cd1ad11d5c357a98e8ecffd66e3d881
James Chen <james.chen@nyu.edu>
2019-08-20 11:19:55 -0400

    added store_hash when react app is loaded to redux

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { STORE_SHOP } from './action/types';
 import thunk from 'redux-thunk';
 import rootReducer from './reducer'
 const initialState = {};
@@ -10,6 +11,14 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, initialState, composeEnhancers(
     applyMiddleware(...middleware)
     ));
+    
+let urlParams = new URLSearchParams(window.location.search);
+const store_hash = urlParams.get('shop').split(".")[0];  
+// console.log(store_hash);
+store.dispatch({
+    type: STORE_SHOP,
+    payload: store_hash
+});
 
 export default store;
 
